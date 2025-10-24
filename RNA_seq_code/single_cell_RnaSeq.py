f89d1f05c978a29c854e4e3887cf8130043fc6a0
richard2049 <133959513+richard2049@users.noreply.github.com>
2025-10-24 16:38:36 +0200

    Update single_cell_RnaSeq.py

--- RNA_seq_code/single_cell_RnaSeq.py
+++ RNA_seq_code/single_cell_RnaSeq.py
@@ -102,4 +102,42 @@ sc.pl.umap(adata, color = ['leiden'])
 
 #Integration
 # You can skip integration step if you only have one sample
+def pp(csv_path):
+    adata = sc.read_csv(csv_path).T
+    sc.pp.filter_genes(adata, min_cells = 10)
+    sc.pp.highly_variable_genes(adata, n_top_genes = 2000, subset = True, flavor = 'seurat_v3')
+    scvi.model.SCVI.setup_anndata(adata)
+    vae = scvi.model.SCVI(adata)
+    vae.train()
+    solo = scvi.external.SOLO.from_scvi_model(vae)
+    solo.train()
+    df = solo.predict()
+    df['prediction'] = solo.predict(soft = False)
+    df.index = df.index.map(lambda x: x[:-2])
+    df['dif'] = df.doublet - df.singlet
+    doublets = df[(df.prediction == 'doublet') & (df.dif > 1)]
+    
+    adata = sc.read_csv(csv_path).T
+    adata.obs['Sample'] = csv_path.split('_')[2] #'raw_counts/GSM5226574_C51ctr_raw_counts.csv' Your data will likely be different. You need a different ID for each sample in the Sample column. In this case I used the .csv path returning the second item  divided by '_' character (the C51ctr part, in this case)  
+    adata.obs['doublet'] = adata.obs.index.isin(doublets.index)
+    adata = adata[~adata.obs.doublet]
+    
+    
+    sc.pp.filter_cells(adata, min_genes=200) #get rid of cells with fewer than 200 genes
+    #sc.pp.filter_genes(adata, min_cells=3) #get rid of genes that are found in fewer than 3 cells
+    adata.var['mt'] = adata.var_names.str.startswith('mt-')  # annotate the group of mitochondrial genes as 'mt'
+    adata.var['ribo'] = adata.var_names.isin(ribo_genes[0].values) # annotate the group of ribosomal genes as 'ribo'
+    sc.pp.calculate_qc_metrics(adata, qc_vars=['mt', 'ribo'], percent_top=None, log1p=False, inplace=True)
+    #Filtering out the outliers
+    upper_lim = np.quantile(adata.obs.n_genes_by_counts.values, .98) 
+    adata = adata[adata.obs.n_genes_by_counts < upper_lim]
+    adata = adata[adata.obs.pct_counts_mt < 20]
+    adata = adata[adata.obs.pct_counts_ribo < 2] 
+
+    return adata
+out=[]
+for file in os.listdir("D:/DATA\GSE171524_RAW/Compressed/"):
+    out.append(pp("D:/DATA\GSE171524_RAW/Compressed/"+file))
+   #List of adata objects (26 in this case)
+
 
